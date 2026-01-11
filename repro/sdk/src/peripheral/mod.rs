9533cee0996f010de3551942f273e8b1c580570f
Jaby <jaby@william.zone>
2026-01-11 09:38:43 +0100

    Remove serial_connection

--- repro/sdk/src/peripheral/mod.rs
+++ repro/sdk/src/peripheral/mod.rs
@@ -1,8 +1,7 @@
 pub mod controller;
-mod serial_connection;
 
 use crate::{
-    peripheral::{controller::{Configuration, ControllerID, ControllerState, RawButtonStates}, serial_connection::{SerialConnection, SerialConnectionError}}, printf
+    peripheral::controller::{Configuration, ControllerID, ControllerState, RawButtonStates}, printf
 };
 pub use controller::RawController;
 
@@ -21,17 +20,19 @@ impl ControllerSlot {
     }
 }
 
+pub struct Dummy {
+} 
+
 
 #[allow(static_mut_refs)]
 #[inline(never)]
 pub fn update_controller() {
-    let mut serial_connection = SerialConnection::activate();
-        process_port(&mut serial_connection, unsafe{&mut CONTROLLERS_A});
-    serial_connection.deactivate();
+    let mut serial_connection = Dummy{};
+    process_port(&mut serial_connection, unsafe{&mut CONTROLLERS_A});
 }
 
-fn process_port(serial_connection: &mut SerialConnection, port_slots: &mut [ControllerSlot; CONTROLLER_SLOT_COUNT]) {
-    //>>> Working <<<
+fn process_port(dummy: &mut Dummy, port_slots: &mut [ControllerSlot; CONTROLLER_SLOT_COUNT]) {
+    // >>> Working <<<
     //for idx in 0..CONTROLLER_SLOT_COUNT {
     //    let slot = &mut port_slots[idx];
     //    process_controller(serial_connection, &mut slot.controller, &slot.configuration);
@@ -39,20 +40,20 @@ fn process_port(serial_connection: &mut SerialConnection, port_slots: &mut [Cont
 
     //>>> All bad <<<
     for slot in port_slots.iter_mut() {
-        process_controller(serial_connection, &mut slot.controller, &slot.configuration);
+        process_controller(dummy, &mut slot.controller, &slot.configuration);
     }
 }
 
 
-fn process_controller(serial_connection: &mut SerialConnection, controller: &mut Option<RawController>, configuration: &Option<Configuration>) {
+fn process_controller(dummy: &mut Dummy, controller: &mut Option<RawController>, configuration: &Option<Configuration>) {
     if let Some(existing_controller) = controller {
-        if let Err(_) = process_existing_controller(serial_connection, existing_controller, configuration) {
+        if let Err(_) = process_existing_controller(dummy, existing_controller, configuration) {
             *controller = None;
         }
     }
 }
 
-fn process_existing_controller(serial_connection: &mut SerialConnection, controller: &mut RawController, configuration: &Option<Configuration>) -> Result<(), SerialConnectionError> {
+fn process_existing_controller(dummy: &mut Dummy, controller: &mut RawController, configuration: &Option<Configuration>) -> Result<(), ()> {
     match controller.get_state() {
         ControllerState::New    => {
             if configuration.is_some() {
@@ -73,7 +74,7 @@ fn process_existing_controller(serial_connection: &mut SerialConnection, control
             } 
             
             // This needs to be like this for the issue to occur
-            serial_connection.enter_config_mode()?;
+            core::hint::black_box(dummy);
             Ok(())
         },
         ControllerState::Stable => {
